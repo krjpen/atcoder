1560ddb715808e80e2f21e6b97f36db577599cf4
Harry Jo <126487712+krjpen@users.noreply.github.com>
2023-07-03 08:46:13 +0900

    Create Echo.cpp

---
+++ Echo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int N;
+	string S;
+	cin >> N;
+	cin >> S;
+
+  
+	for (int i = 0; i < S.size(); i++) {
+		cout << S[i] << S[i];
+	}
+}
