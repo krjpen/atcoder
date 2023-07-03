4708a907bc86ad47e19d10bce5dd74600c4a6d1e
Harry Jo <126487712+krjpen@users.noreply.github.com>
2023-07-03 17:22:18 +0900

    Create Alternately.cpp

---
+++ Alternately.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  string S;
+  cin >> N;
+  cin >> S;
+  bool x = true;
+  vector<string> vec;
+
+  for (int i = 0; i < N - 1; i++) {
+    if (S.at(i) == S.at(i + 1)) {
+      x = false;
+    }
+  }
+
+  if (x == true) {
+    cout << "Yes";
+  } else {
+    cout << "No";
+  }
+}
