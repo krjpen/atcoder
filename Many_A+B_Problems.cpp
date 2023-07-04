e9dfd11fe7f7e97b83054616fd42eee94c43ff35
Harry Jo <126487712+krjpen@users.noreply.github.com>
2023-07-04 13:04:01 +0900

    Create Many_A+B_Problems.cpp

---
+++ Many_A+B_Problems.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  int A, B;
+
+  for (int i = 0; i < N; i++) {
+    cin >> A >> B;
+    cout << A + B << endl;
+  }
+}
