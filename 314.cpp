67675319421a8d84190350143a89c673a5ad2b67
Harry Jo <126487712+krjpen@users.noreply.github.com>
2023-09-10 13:13:13 +0900

    Create 314.cpp

---
+++ 314.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string a = "14159265358979323846264338327950288419716939937510582097494459230"
+             "78164062862089986280348253421170679";
+  int n;
+  cin >> n;
+  cout << "3.";
+  for (int i = 0; i < n; i++) {
+    cout << a[i];
+  }
+}
